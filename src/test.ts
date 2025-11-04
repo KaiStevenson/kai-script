18973bc8202fadd214bde1b1c376a6d54e858883
Kai Stevenson <kai@kaistevenson.com>
2025-11-03 23:47:29 -0800

    better example

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import { evaluate, lex, parse } from "./lang";
 import { createFn } from "./lang";
 
-const adder = createFn("fn(x, add(x, 5))");
+const adder = createFn("fn(x, map(arr(10,20,30), fn(y, add(x, y))))");
 
 const result = adder(5);
 console.log(result);
