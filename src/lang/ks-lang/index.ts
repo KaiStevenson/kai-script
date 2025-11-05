f9f52cd6081036fa98e86a537c925f7a8e924df7
Kai Stevenson <kai@kaistevenson.com>
2025-11-04 20:23:41 -0800

    strict arg count for createFn

--- src/lang/ks-lang/index.ts
+++ src/lang/ks-lang/index.ts
@@ -7,12 +7,17 @@ import {
   EvalError,
   CallFn,
   EmptyStackFrame,
+  ASTNode,
 } from "../ts-lang";
+
+export type TransformArgs<Args extends readonly ASTNode[]> = {
+  [Idx in keyof Args]: any;
+};
 
 export const createFn = <Program extends string>(
   program: Program
 ): Evaluate<Parse<Lex<Program>>> extends [infer ProgramFn extends FnPrim]
-  ? <const Args extends any[]>(
+  ? <const Args extends TransformArgs<ProgramFn["args"]>>(
       ...args: Args
     ) => CallFn<ProgramFn, Args, EmptyStackFrame>
   : EvalError<"Cannot create a function from a program that does not eval to a function"> => {
